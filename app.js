083563824ed0be646a2c040939571e9ccbcf3ccc
arekczap <arekczapski@gmail.com>
2021-02-06 20:58:37 +0100

    added tot nav bar scrolling effect

---
+++ app.js
@@ -0,0 +1,16 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navigation-wrapper").style.top = "0";
+        document.querySelector(".navigation-wrapper").style.backgroundColor = "rgba(84,74,84,.7)";
+
+        if ( window.scrollY == 0 ) {
+            document.querySelector(".navigation-wrapper").style.backgroundColor = "transparent";
+        }
+    } else {
+        document.querySelector(".navigation-wrapper").style.top = "-6rem";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
