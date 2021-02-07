5722b38dc2cd6dcb3bc4687af074d9803cd887ed
arekczap <arekczapski@gmail.com>
2021-02-08 00:27:30 +0100

    finished rwd for tablet and small desktop, to do large desktop

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+    let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.querySelector(".navigation-wrapper").style.top = "0";
         document.querySelector(".navigation-wrapper").style.backgroundColor = "rgba(84,74,84,.7)";
 
-        if ( window.scrollY == 0 ) {
+        if ( window.scrollY === 0 ) {
             document.querySelector(".navigation-wrapper").style.backgroundColor = "transparent";
         }
     } else {
