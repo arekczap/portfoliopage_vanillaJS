cf3ec578ab240a747724bc1236116355c756387d
Arkadiusz Czapski <acza@sbs.wikpol.com.pl>
2021-02-12 15:02:02 +0100

    finished scroll for nav and header on desktop and mobile

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
 let hamburgerNavigation = document.querySelector(".navigation-global");
 let navigationWrapper =  document.querySelector(".navigation-wrapper");
 
-// let prevScrollPos = window.pageYOffset;
-
-
-
-// window.onscroll = function() {
-//     let currentScrollPos = window.pageYOffset;
-//     if (prevScrollPos > currentScrollPos) {
-//         navigationWrapper.style.top = "0";
-//         navigationWrapper.style.backgroundColor = "rgba(84,74,84,.7)";
-//
-//         if ( window.scrollY === 0 ) {
-//             navigationWrapper.style.backgroundColor = "transparent";
-//         }
-//     } else if (prevScrollPos < currentScrollPos) {
-//         navigationWrapper.style.top = "-6rem";
-//
-//     }
-//     prevScrollPos = currentScrollPos;
-// }
-//
 let scrollDir = 0;
 let canScroll = true;
+let touchStart = 0;
+let touchEnd = 0;
 
-
-if ( window.scrollY > 0 ) {
-    navigationWrapper.style.top = "-6rem";
-}
 
 document.addEventListener('wheel', (event) => {
-
     if (!canScroll) {
         return;
     }
@@ -39,15 +16,28 @@ document.addEventListener('wheel', (event) => {
 
     setTimeout(() => {
         canScroll = true;
-    }, 300);
+    }, 100);
 
-    scrollDir = event.deltaY > 1 ? 1 : -1;
-    scrollTopNavigation(scrollDir)
+    scrollDir = event.deltaY > 0 ? 1 : -1;
+    scrollTopNavigation(scrollDir);
+});
+
+document.addEventListener('touchstart', (event) => {
+    touchStart = event.changedTouches[0].clientY;
+});
+
+document.addEventListener('touchend', (event) => {
+    touchEnd = event.changedTouches[0].clientY;
+    if (touchStart > touchEnd) {
+        scrollTopNavigation(1);
+    } else {
+        scrollTopNavigation(-1);
+    }
 });
 
 
 let scrollTopNavigation = (actualScrollDirection) => {
-    if (actualScrollDirection === 1) {
+    if (actualScrollDirection === 1 && hamburgerNavigation.dataset.active === "no") {
         navigationWrapper.style.top = "-6rem";
 
     } else {
@@ -58,24 +48,31 @@ let scrollTopNavigation = (actualScrollDirection) => {
             navigationWrapper.style.backgroundColor = "transparent";
         }
     }
+
 };
 
 let hideNavBar = () => {
+    navigationWrapper.style.top = "-6rem";
+
     if (window.scrollY !== 0) {
-        navigationWrapper.style.top = "0";
-        console.log("zadzialalo")
-    } else {
-        navigationWrapper.style.top = "-6rem";
-        console.log("zadzialasdasdaso")
+        navigationWrapper.style.backgroundColor = "transparent";
     }
 }
 
+    let showNavBar = () => {
+        navigationWrapper.style.top = "0";
+        navigationWrapper.style.backgroundColor = "transparent";
+    }
 
 let hideAndShowNav = () => {
-
-    if (hamburgerNavigation.style.top === "-100vh") {
-        hamburgerNavigation.style.top = "0"
-    } else {
-        hamburgerNavigation.style.top = "-100vh"
+     if (hamburgerNavigation.style.top === "-100vh") {
+         hamburgerNavigation.style.top = "0"
+         document.body.classList.add("transparent-scrollBar");
+         hamburgerNavigation.dataset.active = "yes";
+     } else {
+        hamburgerNavigation.style.top = "-100vh";
+        document.body.style.overflowY = "visible";
+         document.body.classList.remove("transparent-scrollBar");
+        hamburgerNavigation.dataset.active = "no";
     }
 }
