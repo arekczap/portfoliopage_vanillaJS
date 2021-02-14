12083725c8c5e2af14d066787c274123c37a3b91
arekczap <arekczapski@gmail.com>
2021-02-14 20:41:08 +0100

    final code,refactored, deleted comments etc

--- app.js
+++ app.js
@@ -1,78 +1,83 @@
-let hamburgerNavigation = document.querySelector(".navigation-global");
-let navigationWrapper =  document.querySelector(".navigation-wrapper");
-
-let scrollDir = 0;
-let canScroll = true;
-let touchStart = 0;
-let touchEnd = 0;
-
-
-document.addEventListener('wheel', (event) => {
-    if (!canScroll) {
-        return;
+    let selectors = {
+        hamburgerNavigation : document.querySelector(".navigation-global"),
+        navigationWrapper : document.querySelector(".navigation-wrapper")
     }
 
-    canScroll = false;
+    let scrollDir = 0;
+    let canScroll = true;
+    let touchStart = 0;
+    let touchEnd = 0;
 
-    setTimeout(() => {
-        canScroll = true;
-    }, 100);
+    document.addEventListener('wheel', (event) => {
+        if (!canScroll) {
+            return;
+        }
+        canScroll = false;
 
-    scrollDir = event.deltaY > 0 ? 1 : -1;
-    scrollTopNavigation(scrollDir);
-});
+        setTimeout(() => {
+            canScroll = true;
+        }, 100);
 
-document.addEventListener('touchstart', (event) => {
-    touchStart = event.changedTouches[0].clientY;
-});
+        scrollDir = event.deltaY > 0 ? 1 : -1;
+        scrollTopNavigation(scrollDir);
+    });
 
-document.addEventListener('touchend', (event) => {
-    touchEnd = event.changedTouches[0].clientY;
-    if (touchStart > touchEnd) {
-        scrollTopNavigation(1);
-    } else {
-        scrollTopNavigation(-1);
-    }
-});
+    document.addEventListener('touchstart', (event) => {
+        touchStart = event.changedTouches[0].clientY;
+    });
 
+    document.addEventListener('touchend', (event) => {
+        touchEnd = event.changedTouches[0].clientY;
+        if (touchStart > touchEnd) {
+            scrollTopNavigation(1);
+        } else {
+            scrollTopNavigation(-1);
+        }
+    });
 
-let scrollTopNavigation = (actualScrollDirection) => {
-    if (actualScrollDirection === 1 && hamburgerNavigation.dataset.active === "no") {
+    let scrollTopNavigation = (actualScrollDirection) => {
+        let {navigationWrapper, hamburgerNavigation} = selectors;
+
+        if (actualScrollDirection === 1 && hamburgerNavigation.dataset.active === "no") {
+            navigationWrapper.style.top = "-6rem";
+
+        } else {
+            navigationWrapper.style.top = "0";
+            navigationWrapper.style.backgroundColor = "rgba(84,74,84,.7)";
+
+            if ( window.scrollY === 0 ) {
+                navigationWrapper.style.backgroundColor = "transparent";
+            }
+        }
+    };
+
+    let hideNavBar = () => {
+        let {navigationWrapper} = selectors;
         navigationWrapper.style.top = "-6rem";
 
-    } else {
-        navigationWrapper.style.top = "0";
-        navigationWrapper.style.backgroundColor = "rgba(84,74,84,.7)";
-
-        if ( window.scrollY === 0 ) {
+        if (window.scrollY !== 0) {
             navigationWrapper.style.backgroundColor = "transparent";
         }
     }
 
-};
-
-let hideNavBar = () => {
-    navigationWrapper.style.top = "-6rem";
-
-    if (window.scrollY !== 0) {
-        navigationWrapper.style.backgroundColor = "transparent";
-    }
-}
-
     let showNavBar = () => {
+        let {navigationWrapper} = selectors;
+
         navigationWrapper.style.top = "0";
         navigationWrapper.style.backgroundColor = "transparent";
     }
 
-let hideAndShowNav = () => {
-     if (hamburgerNavigation.style.top === "-100vh") {
-         hamburgerNavigation.style.top = "0"
-         document.body.classList.add("transparent-scrollBar");
-         hamburgerNavigation.dataset.active = "yes";
-     } else {
-        hamburgerNavigation.style.top = "-100vh";
-        document.body.style.overflowY = "visible";
-         document.body.classList.remove("transparent-scrollBar");
-        hamburgerNavigation.dataset.active = "no";
+    let hideAndShowNav = () => {
+        let {hamburgerNavigation} = selectors;
+
+         if (hamburgerNavigation.style.top === "-100vh") {
+             document.body.classList.add("transparent-scrollBar");
+             hamburgerNavigation.style.top = "0"
+             hamburgerNavigation.dataset.active = "yes";
+         } else {
+            hamburgerNavigation.style.top = "-100vh";
+            document.body.classList.remove("transparent-scrollBar");
+            document.body.style.overflowY = "visible";
+            hamburgerNavigation.dataset.active = "no";
+        }
     }
-}
